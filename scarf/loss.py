0d5713dad601cf793321c030dbe651b17532b8eb
Clement Labrugere <clement.labrugere@decathlon.com>
2022-07-29 09:48:39 +0200

    use torch.mm instead of @ for cosine similarity

--- scarf/loss.py
+++ scarf/loss.py
@@ -10,12 +10,11 @@ class InfoNCE(nn.Module):
         self.reduction = reduction
 
     def forward(self, embeddings, embeddings_corrupted):
-        # normalize the embedding vector so that they have unit norm
+        # compute pairwise cosine similarity betwteen embeddings
         embeddings = F.normalize(embeddings, p=2, dim=1)
         embeddings_corrupted = F.normalize(embeddings_corrupted, p=2, dim=1)
 
-        # compute pairwise cosine similarity betwteen embeddings
-        similarity = embeddings @ embeddings_corrupted.transpose(-2, -1)
+        similarity = torch.mm(embeddings, embeddings.transpose(0, 1))
 
         # positive samples
         positive = torch.arange(len(embeddings), device=embeddings.device)
